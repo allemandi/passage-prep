7fb6f624fc75c947ee9520f9a60274ed6ad47d02
allemandi <69766017+allemandi@users.noreply.github.com>
2025-04-12 22:07:31 +0100

    sort questionTable by book index

--- src/components/QuestionTable.js
+++ src/components/QuestionTable.js
@@ -9,8 +9,21 @@ import {
   TableRow,
   Checkbox,
 } from '@mui/material';
+import bibleCounts from '../data/bible-counts.json'; // Adjust the path as necessary
 
 const QuestionTable = ({ questions, selectedQuestions, onQuestionSelect }) => {
+  // Sort questions by book index and then by chapter
+  const sortedQuestions = questions.sort((a, b) => {
+    const bookAIndex = bibleCounts[a.biblePassage.split(' ')[0]] || Infinity; // Get index of book or Infinity if not found
+    const bookBIndex = bibleCounts[b.biblePassage.split(' ')[0]] || Infinity;
+
+    if (bookAIndex === bookBIndex) {
+      // If books are the same, sort by chapter
+      return (parseInt(a.biblePassage.split(' ')[1]) || 0) - (parseInt(b.biblePassage.split(' ')[1]) || 0);
+    }
+    return bookAIndex - bookBIndex; // Sort by book index
+  });
+
   return (
     <TableContainer component={Paper} sx={{ mt: 2, maxHeight: 400 }}>
       <Table stickyHeader>
@@ -23,7 +36,7 @@ const QuestionTable = ({ questions, selectedQuestions, onQuestionSelect }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {questions.map((question, index) => (
+          {sortedQuestions.map((question, index) => (
             <TableRow key={index}>
               <TableCell padding="checkbox">
                 <Checkbox
