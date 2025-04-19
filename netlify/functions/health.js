335890f6ba8110ea2a009d0c2d4ae40a68e4efa0
allemandi <69766017+allemandi@users.noreply.github.com>
2025-04-19 20:56:10 +0100

    stale import fix

--- netlify/functions/health.js
+++ netlify/functions/health.js
@@ -1,4 +1,4 @@
-const { connectToDatabase } = require('./utils/db');
+const { connectToDatabase } = require('../../src/utils/server');
 
 // Health Check Function
 exports.handler = async function(event, context) {
